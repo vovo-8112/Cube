c64ca503430183f6ac31a6e2a22cc2d7a7cff6eb
Albert.Medow <polpybert@gmail.com>
2021-07-11 14:49:16 +0300

    fix reload button and changes for background image

--- Assets/Scripts/ButtonReload.cs
+++ Assets/Scripts/ButtonReload.cs
@@ -3,11 +3,13 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonReload : MonoBehaviour {
-  private void Awake() {
+  private void Awake() 
+  {
     GetComponent<Button>().onClick.AddListener(ReloadScene);
   }
 
-  private void ReloadScene() {
+  private void ReloadScene() 
+  {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
   }
 }
